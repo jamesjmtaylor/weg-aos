ac95b29bbd841487063b859f24b9fa5fec1abfd5
James Taylor <james.jm.taylor@gmail.com>
2018-02-21 05:41:11 -0800

    Added calculator and cards fragments and layouts

---
+++ app/src/main/java/com/jamesjmtaylor/weg2015/tabBar/equipmentTabs/EquipmentActivity.kt
@@ -0,0 +1,13 @@
+package com.jamesjmtaylor.weg2015.tabBar.equipmentTabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jamesjmtaylor.weg2015.R
+
+class EquipmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_equipment)
+    }
+}
