70eda24d7c50a90fc8e3f376b12ebb83ae63ba04
James Taylor <james.jm.taylor@gmail.com>
2018-03-16 05:41:51 -0700

    Added basic fragment layouts for flashcards

--- app/src/main/java/com/jamesjmtaylor/weg2015/tabBar/cardsTab/CardsFragment.kt
+++ app/src/main/java/com/jamesjmtaylor/weg2015/tabBar/cardsTab/CardsFragment.kt
@@ -21,7 +21,7 @@ class CardsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cards, container, false)
+        return inflater.inflate(R.layout.fragment_cards_setup, container, false)
     }
 
     override fun onResume() {
